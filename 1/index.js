03129828ac427460df522326e1c0368a4d21b4c6
nakegunmo <67962022+nakegunmo@users.noreply.github.com>
2024-05-08 13:53:51 +0900

    Delete 1 directory

--- 1/index.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('header a').click(function() {
-    let id = $(this).attr("href")
-    let position = $(id).offset().top;
-    $('html, body').animate({
-      'scrollTop':position-70
-    },200)
-  })
-});
